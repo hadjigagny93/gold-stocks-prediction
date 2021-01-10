f30dc21b13db6d49c7ab46c88cbbeba97b70375a
hadjigagny93 <hadjigagny93@gmail.com>
2021-01-10 06:42:45 +0100

    Create pkg dir in src etl source

--- src/pkg/settings.py
+++ src/pkg/settings.py
@@ -1,7 +1,7 @@
 import os
 
 # DEFAULT DIRS
-REPO_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+REPO_DIR = os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), '..'), '..'))
 CONFIG_PATH = os.path.join(REPO_DIR, 'config/browser.yaml')
 DATA_DIR = os.path.join(REPO_DIR, 'data/news')
 
