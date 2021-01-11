293c02e859cd62fbf32a1a4bc914258a884d3274
hadjigagny93 <hadjigagny93@gmail.com>
2021-01-11 07:00:00 +0100

    Import models in ingest.py

--- backend/apis/ingest.py
+++ backend/apis/ingest.py
@@ -1,3 +1,6 @@
+from .models import *
+import datetime
+
 
 class Import(type):
     def __call__(cls, import_task):
@@ -5,14 +8,16 @@ class Import(type):
             if import_task['transfer_mode'] == 0: return BackImportTask(import_task)
             elif import_task['transfer_mode'] == 1: return BackImportTask(import_task)
             else:
-                raise NotImplementedError("This autoML paradigm is not yet implemented.")
+                raise NotImplementedError("This transfer mode paradigm is not yet implemented.")
         return super().__call__(automl_import_task)
 
 class ImportTask(object, metaclass=Import):
 
     def __init__(self, import_task, **kwargs):
         self.transfer_mode = import_task.get('transfer_mode')
+        self.current_datetime = datetime.datetime.now()
 
+    
      def transfer(self):
          raise NotImplementedError("This method has not been implemented yet")
 
